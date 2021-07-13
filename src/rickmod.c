3d1fdea31f9d4ef4286709543db87e75c21a2a3f
Steven Arnow <s@rdw.se>
2021-07-13 23:11:56 +0200

    Fix length of valid notes list

--- src/rickmod.c
+++ src/rickmod.c
@@ -19,7 +19,7 @@
 int rand(void);
 #endif
 
-static uint16_t valid_notes[48] = {
+static uint16_t valid_notes[36] = {
 	856, 808, 762, 720, 678, 640, 604, 570, 538, 508, 480, 453,
 	428, 404, 381, 360, 339, 320, 302, 285, 269, 254, 240, 226,
 	214, 202, 190, 180, 170, 160, 151, 143, 135, 127, 120, 113,
@@ -841,7 +841,7 @@ int rm_save(struct RickmodState *rm, const char *path) {
 int rm_lookup_note(int note) {
 	int i;
 
-	for (i = 0; i < 48; i++)
+	for (i = 0; i < 36; i++)
 		if (note == valid_notes[i])
 			return i;
 	return -1;
